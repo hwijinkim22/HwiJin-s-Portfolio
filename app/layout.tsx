b92e0346c798aaa4e5511f7f0cd6ab7da3e9ae46
hwijinkim22 <bout9396@naver.com>
2025-01-04 00:52:09 +0900

    remove: layout.css에서 작은 따옴표 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "Hwijin's Portfolio",
+  title: "Hwijin Portfolio",
   description: 'Frontend Developer Portfolio',
 }
 
